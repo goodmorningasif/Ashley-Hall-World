ebe4f1867effa19ad7dffcc19460ab9afcc14f84
Asif Ahmed <asif@stitchdesignco.com>
2016-12-13 15:11:37 -0500

    Add theme files

---
+++ content/themes/ahw/404.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package SDCOPartners
+ * @subpackage awh
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+
+<?php get_footer();
