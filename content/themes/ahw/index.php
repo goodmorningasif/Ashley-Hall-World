04dec2ec4ea38bdc1b8e30722d109aa0c7274b80
Asif Ahmed <asif@stitchdesignco.com>
2017-01-05 16:11:02 -0500

    Build out blog feed

--- content/themes/ahw/index.php
+++ content/themes/ahw/index.php
@@ -25,45 +25,48 @@ get_header(); ?>
       <div id="logo-main"><?php echo file_get_contents($GLOBALS['url']."/assets/logo-main.svg"); ?></div>
       <div id="logo-seal"><?php echo file_get_contents($GLOBALS['url']."/assets/logo-seal.svg"); ?></div>
     </div>
-    <div id="discovery-map--logo">Logo</div>
+    <div id="discovery-map--logo"></div>
   </div>
   
-  <!-- ====  Block: Blog Feed  ==== -->
-  <div id="blog-feed">
-    <h2>A Day In The Life</h2>
-
-    <div class="blog-feed--lcol">
-      <div class="image" id="lcol-0">
-        <div class="play-bttn"><?php echo file_get_contents($GLOBALS['url']."/assets/play-bttn.svg"); ?></div>
-      </div>
-      <h3>Learn</h3>
-      <h4>Headline Goes Here</h4>
-      <p>sedut perspiciatis unde omnis iste natus.</p>
-    </div>
-			
-			<div class="blog-feed--rcol">
-      <div class="image" id="rcol-0">
-        <div class="play-bttn"><?php echo file_get_contents($GLOBALS['url']."/assets/play-bttn.svg"); ?></div>
-      </div>
-      <h3>Play</h3>
-      <h4>Headline Here</h4>
-      <p>sedut perspiciatis unde omnis iste natus.</p>
-			</div>
-
+  <!-- ====  Block: Feeds  ==== -->
+  <div id="pre-feeds">
+    <h2>A Day In The Life...</h2>
   </div>
 
+  <div id="feeds">
 
-  <!-- ====  Block: Social Feed  ==== -->
-  <div id="social-feed">
-    <h2>Learn Like A Girl</h2>
-
-    <div class="social-feed--col">
-      <div class="image" id="scol-0">
-        <div class="play-bttn"><?php echo file_get_contents($GLOBALS['url']."/assets/play-bttn.svg"); ?></div>
+    <div id="feeds--blog-feed">
+      <div class="feeds--repeater">
+        <div class="feeds--lcol">
+          <div class="image" id="lcol-0">
+            <div class="play-bttn"><?php echo file_get_contents($GLOBALS['url']."/assets/play-bttn.svg"); ?></div>
+          </div>
+          <span class="label">Learn</span>
+          <h3>Headline Goes Here</h3>
+          <p>sedut perspiciatis unde omnis iste natus.</p>
+        </div>
+    			
+    		<div class="feeds--rcol">
+          <div class="image" id="rcol-0">
+            <div class="play-bttn"><?php echo file_get_contents($GLOBALS['url']."/assets/play-bttn.svg"); ?></div>
+          </div>
+          <span class="label">Play</span>
+          <h3>Headline Here</h3>
+          <p>sedut perspiciatis unde omnis iste natus.</p>
+    		</div>
       </div>
-      <h3>History</h3>
-      <h4>Mcbee Plaid</h4>
-      <p>sedut perspiciatis unde omnis iste natus.</p>
+    </div>
+
+    <div id="feeds--social-feed">
+      <h2>Learn Like A Girl</h2>
+      <div class="social-feed--col">
+        <div class="image" id="scol-0">
+          <div class="play-bttn"><?php echo file_get_contents($GLOBALS['url']."/assets/play-bttn.svg"); ?></div>
+        </div>
+        <span class="label">History</span>
+        <h3>Mcbee Plaid</h3>
+        <p>sedut perspiciatis unde omnis iste natus.</p>
+      </div>
     </div>
   </div>
 
