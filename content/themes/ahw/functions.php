05865efea742e63a0ae0865f5fdce7dcc4c90643
Asif Ahmed <asif@stitchdesignco.com>
2017-01-10 09:53:56 -0500

    Clear spaces from Functions

--- content/themes/ahw/functions.php
+++ content/themes/ahw/functions.php
@@ -5,10 +5,20 @@
 
 
 	/* Add Styles, Fonts, and Javascript */
-	wp_enqueue_scripts();
-	
-	//<link rel="stylesheet" type="text/css" href="https://cloud.typography.com/778678/6993372/css/fonts.css" />
-  wp_enqueue_style('cloud.typography', 'https://cloud.typography.com/778678/6993372/css/fonts.css');
-	wp_enqueue_style('styles', $GLOBALS['url'].'/prod/styles.css');
+  wp_enqueue_scripts();
+
+	function my_enqueue_style() {
+
+		//<link rel="stylesheet" type="text/css" href="https://cloud.typography.com/778678/6993372/css/fonts.css" />
+	  wp_enqueue_style('cloud.typography', 'https://cloud.typography.com/778678/6993372/css/fonts.css');
+		wp_enqueue_style('styles', $GLOBALS['url'].'/prod/styles.css');
+
+		add_theme_support( 'post-thumbnails' );
+
+  }
+
+  add_action( 'wp_enqueue_scripts', 'my_enqueue_style' );
+
+  add_theme_support( 'post-thumbnails' );
 
 ?>
